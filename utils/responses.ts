b41798f86a210fc3903f5ff0ba88631968c457ac
Pavel Protchenko <protchenkopa@gmail.com>
2026-04-13 12:07:11 +0300

    setup express router and renamed some spelling errors

---
+++ utils/responses.ts
@@ -0,0 +1,16 @@
+import type { Response } from "express";
+
+export const successResponse = (res: Response, data: any, message: string = "Success") => {
+    res.status(200).json({
+        success: true,
+        message,
+        data
+    });
+};
+
+export const errorResponse = (res: Response, status: number, error: string) => {
+    res.status(status).json({
+        success: false,
+        error
+    });
+};
